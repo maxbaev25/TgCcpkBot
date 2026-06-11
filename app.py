8dc15cbc45a8a7203eef61f1ca0960f3893b3374
Flapchik <58729268+Flapchik@users.noreply.github.com>
2026-06-11 14:43:37 +0300

    Add BASE_URL

--- app.py
+++ app.py
@@ -4,10 +4,12 @@ import requests
 from pprint import pprint, pp
 from datetime import datetime, date
 
+BASE_URL = "https://backend.cppktrain.ru
+
 
 def get_all_trains(
         from_station_id: int, to_station_id: int, origin_date: date) -> list[dict]:
-    url = "https://backend.cppktrain.ru/train-schedule/date-travel"
+    url = f"{BASE_URL}/train-schedule/date-travel"
     params = {
         "date": origin_date,
         "fromStationId": from_station_id,
@@ -19,7 +21,7 @@ def get_all_trains(
 
 def get_all_free_trains(
         from_station_id: int, to_station_id: int, origin_date: date) -> list[dict]:
-    url = "https://backend.cppktrain.ru/api/TrainPricing"
+    url = f"{BASE_URL}/api/TrainPricing"
     params = {
         "departureDate": origin_date.strftime("%Y-%m-%d"),
         "originCode": from_station_id,
@@ -31,7 +33,7 @@ def get_all_free_trains(
 
 def get_all_train_places(
         train_id: int, from_station_id: int, to_station_id: int, origin_date: date) -> dict:
-    url = "https://backend.cppktrain.ru/api/CarPricing"
+    url = f"{BASE_URL}/api/CarPricing"
     params = {
         "departuredate": "2026-06-11",
         "origincode": from_station_id,
